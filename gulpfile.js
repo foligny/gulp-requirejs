e9b903f1aba3880df3b4ba049e3b82280c8669dd
bornkiller <hjj491229492@hotmail.com>
2014-09-10 16:36:33 +0800

    set up basic structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var gulpRequirejs = require('./index.js');
+
+gulp.task('manual', function(){
+	return gulp.src([
+		'modules/**/*.js'
+	])
+	.pipe(gulpRequirejs({}))
+    .pipe(gulp.dest('dist'))
+});
 
 gulp.task('jshint', function() {
 	return gulp.src([
 		'*.js',
 		'!gulpfile.js',
 		'test/**/*.js',
-		'!test/fixtures/**/*.js',
+		'!test/fixtures/**/*.js'
 	])
 	.pipe(jshint())
 	.pipe(jshint.reporter(stylish))
-})
+});
 
 gulp.task('test', ['jshint'], function() {
-	return ;
-})
+	return true;
+});
---
+++ index.js
@@ -0,0 +1,47 @@
+var gutil = require('gulp-util');
+var through = require('through');
+var path = require('path');
+var merge = require('merge');
+var PluginError = gutil.PluginError;
+var PLUGIN_NAME = 'gulp-requirejs-optimizer';
+
+module.exports = function(opts) {
+  if (!opts) {
+  	throw new PluginError(PLUGIN_NAME, 'Missing options for gulp-requirejs-optimizer');
+  }
+
+  var moduleStorage = {};
+
+  function transformBuffer(file) {
+  	if (file.isNull()) {
+  	  return false;
+  	}
+
+  	if (file.isStream()) {
+      return this.emit('error', new PluginError(PLUGIN_NAME, 'stream not supported'));
+  	}
+
+    var options = merge({
+      baseUrl: './modules',
+      path: {},
+      shim: {}
+    }, opts);
+
+    /*
+     * To get the same relative path, and storage file content as K-V
+     */
+    var fullBaseUrl = path.resolve(file.cwd, options.baseUrl);
+    var relativeUrl = gutil.replaceExtension(path.relative(fullBaseUrl, file.path), '');
+    moduleStorage[relativeUrl] = file;
+  }
+
+  function flushBuffer() {
+      this.emit('data', moduleStorage['b']);
+      this.emit('end');
+  }
+
+  return through(transformBuffer, flushBuffer);
+};
+
+
+
