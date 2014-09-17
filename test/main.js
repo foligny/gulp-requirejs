5d3600e59aeffc5fe49b7030a97e666287550313
bornkiller <hjj491229492@hotmail.com>
2014-09-17 11:35:59 +0800

    modify test cases.

--- test/main.js
+++ test/main.js
@@ -8,99 +8,99 @@ var gulpRequirejs = require('../index.js');
 require('mocha');
 require('should');
 
-//describe('gulp-requirejs-errors', function () {
-//    it('should throw when options is missing', function () {
-//        (function() {
-//            gulp.src(['./test/fixtures/lang.js']).pipe(gulpRequirejs());
-//        })
-//        .should.throw('Missing options for gulp-requirejs-optimizer');
-//    });
-//
-//    it('should emit error when streamed file', function (done) {
-//        gulp.src(['./test/fixtures/lang.js'], {buffer: false})
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixture',
-//                module: 'main'
-//            }))
-//            .on('error', function(err) {
-//                err.message.should.eql('Stream not supported');
-//                done();
-//            });
-//    });
-//
-//    it('should emit error when missing required modules', function (done) {
-//        gulp.src('./test/fixtures/*.js')
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixtures',
-//                module: 'optimize-error'
-//            }))
-//            .on('error', function(err) {
-//                err.message.should.eql('Missing required module love in optimize-error');
-//                done();
-//            });
-//    });
-//});
+describe('gulp-requirejs-errors', function () {
+   it('should throw when options is missing', function () {
+       (function() {
+           gulp.src(['./test/fixtures/lang.js']).pipe(gulpRequirejs());
+       })
+       .should.throw('Missing options for gulp-requirejs-optimizer');
+   });
 
-//describe('gulp-requirejs-optimize', function () {
-//    it('should optimize single file without sub-directory', function(done) {
-//        gulp.src('./test/fixtures/*.js')
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixtures',
-//                module: 'optimize-info'
-//            }))
-//            .pipe(assert.first(function(file){
-//                var compare = "define('lang',[],function(){});define('logger',[],function(){});define('optimize-info',['lang','logger'],function(lang,logger){});";
-//                (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
-//            }))
-//            .on('end', done);
-//    });
-//
-//    it('should optimize single file with sub-directory', function(done) {
-//        gulp.src('./test/fixtures/**/*.js')
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixtures',
-//                module: 'optimize-sub'
-//            }))
-//            .pipe(assert.first(function(file){
-//                var compare = "define('lang',[],function(){});define('subFixtures/html',[],function(){});define('optimize-sub',['lang','subFixtures/html'],function(lang,html){});";
-//                (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
-//            }))
-//            .on('end', done);
-//    });
-//
-//    it('should optimize file with path config', function(done) {
-//        gulp.src('./test/fixtures/**/*.js')
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixtures',
-//                path: {
-//                    'html': 'subFixtures/html'
-//                },
-//                module: 'optimize-path'
-//            }))
-//            .pipe(assert.first(function(file){
-//                var compare = "define('lang',[],function(){});define('html',[],function(){});define('optimize-path',['lang','html'],function(lang,html){});";
-//                (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
-//            }))
-//            .on('end', done);
-//    });
-//
-//    it('should optimize multiple file', function(done) {
-//        gulp.src('./test/fixtures/**/*.js')
-//            .pipe(gulpRequirejs({
-//                baseUrl: './test/fixtures',
-//                module: ['optimize-info','optimize-sub']
-//            }))
-//            .pipe(assert.first(function(file){
-//                var compare = "define('lang',[],function(){});define('logger',[],function(){});define('optimize-info',['lang','logger'],function(lang,logger){});";
-//                (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
-//            }))
-//            .pipe(assert.last(function(file){
-//                var compare = "define('lang',[],function(){});define('subFixtures/html',[],function(){});define('optimize-sub',['lang','subFixtures/html'],function(lang,html){});";
-//                (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
-//            }))
-//            .on('end', done);
-//    });
-//});
+   it('should emit error when streamed file', function (done) {
+       gulp.src(['./test/fixtures/lang.js'], {buffer: false})
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixture',
+               module: 'main'
+           }))
+           .on('error', function(err) {
+               err.message.should.eql('Stream not supported');
+               done();
+           });
+   });
+
+   it('should emit error when missing required modules', function (done) {
+       gulp.src('./test/fixtures/*.js')
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixtures',
+               module: 'optimize-error'
+           }))
+           .on('error', function(err) {
+               err.message.should.eql('Missing required module love in optimize-error');
+               done();
+           });
+   });
+});
+
+describe('gulp-requirejs-optimize', function () {
+   it('should optimize single file without sub-directory', function(done) {
+       gulp.src('./test/fixtures/*.js')
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixtures',
+               module: 'optimize-info'
+           }))
+           .pipe(assert.first(function(file){
+               var compare = "define('lang',[],function(){});define('logger',[],function(){});define('optimize-info',['lang','logger'],function(lang,logger){});";
+               (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
+           }))
+           .on('end', done);
+   });
+
+   it('should optimize single file with sub-directory', function(done) {
+       gulp.src('./test/fixtures/**/*.js')
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixtures',
+               module: 'optimize-sub'
+           }))
+           .pipe(assert.first(function(file){
+               var compare = "define('lang',[],function(){});define('subFixtures/html',[],function(){});define('optimize-sub',['lang','subFixtures/html'],function(lang,html){});";
+               (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
+           }))
+           .on('end', done);
+   });
+
+   it('should optimize file with path config', function(done) {
+       gulp.src('./test/fixtures/**/*.js')
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixtures',
+               path: {
+                   'html': 'subFixtures/html'
+               },
+               module: 'optimize-path'
+           }))
+           .pipe(assert.first(function(file){
+               var compare = "define('lang',[],function(){});define('html',[],function(){});define('optimize-path',['lang','html'],function(lang,html){});";
+               (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
+           }))
+           .on('end', done);
+   });
+
+   it('should optimize multiple file', function(done) {
+       gulp.src('./test/fixtures/**/*.js')
+           .pipe(gulpRequirejs({
+               baseUrl: './test/fixtures',
+               module: ['optimize-info','optimize-sub']
+           }))
+           .pipe(assert.first(function(file){
+               var compare = "define('lang',[],function(){});define('logger',[],function(){});define('optimize-info',['lang','logger'],function(lang,logger){});";
+               (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
+           }))
+           .pipe(assert.last(function(file){
+               var compare = "define('lang',[],function(){});define('subFixtures/html',[],function(){});define('optimize-sub',['lang','subFixtures/html'],function(lang,html){});";
+               (file.contents.toString().replace(/\s*/g, '')).should.eql(compare);
+           }))
+           .on('end', done);
+   });
+});
 
 describe('parse module', function () {
     var sample;
@@ -178,29 +178,29 @@ describe('parse module', function () {
     });
 });
 
-//describe('normalize path module', function () {
-//    it('should normalize file path', function (done) {
-//        gulp.src(['./test/fixtures/lang.js'])
-//            .pipe(through(function(file, encoding, callback) {
-//                var basePath = './test';
-//                var fullBasePath = path.resolve(file.cwd, basePath);
-//                var relativePath = normalizer.normalizeFileRelative(fullBasePath, file.path);
-//                relativePath.should.equal(path.join('fixtures', 'lang'));
-//                callback();
-//                done();
-//            }));
-//    });
-//
-//    it('should normalize dependent path', function (done) {
-//        gulp.src(['./test/fixtures/lang.js'])
-//            .pipe(through(function(file, encoding, callback) {
-//                var basePath = './test';
-//                var fullBasePath = path.resolve(file.cwd, basePath);
-//                var fileRelativePath = normalizer.normalizeFileRelative(fullBasePath, file.path);
-//                var dependentRelativePath = normalizer.normalizeDependentRelative(fullBasePath, './fixtures/lang');
-//                fileRelativePath.should.equal(dependentRelativePath);
-//                callback();
-//                done();
-//            }));
-//    });
-//});
+describe('normalize path module', function () {
+   it('should normalize file path', function (done) {
+       gulp.src(['./test/fixtures/lang.js'])
+           .pipe(through(function(file, encoding, callback) {
+               var basePath = './test';
+               var fullBasePath = path.resolve(file.cwd, basePath);
+               var relativePath = normalizer.normalizeFileRelative(fullBasePath, file.path);
+               relativePath.should.equal(path.join('fixtures', 'lang'));
+               callback();
+               done();
+           }));
+   });
+
+   it('should normalize dependent path', function (done) {
+       gulp.src(['./test/fixtures/lang.js'])
+           .pipe(through(function(file, encoding, callback) {
+               var basePath = './test';
+               var fullBasePath = path.resolve(file.cwd, basePath);
+               var fileRelativePath = normalizer.normalizeFileRelative(fullBasePath, file.path);
+               var dependentRelativePath = normalizer.normalizeDependentRelative(fullBasePath, './fixtures/lang');
+               fileRelativePath.should.equal(dependentRelativePath);
+               callback();
+               done();
+           }));
+   });
+});
